ed98bf45356b60b440dc7dbb83568e79ed1462cd
ozelot379 <ozelot379@icloud.com>
2019-08-21 16:59:25 +0200

    Restruct scripts and removed unused tests

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-const assert = require("assert");
-const ConvertMinecraftJavaTextureToBedrock = require("../");
-
-describe("ConvertMinecraftJavaTextureToBedrock", () => {
-
-	console.log("Currently no tests are planed :(");
-});
